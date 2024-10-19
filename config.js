045f5730a1baaddb7fc4dd95a5f1dd3c6c4fdbe1
TrinhTrungKien1 <kien98798@gmail.com>
2024-10-19 23:57:23 +0700

    Add files via upload
    mymom

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Mẹ';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100011552926373';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Gửi mẹ';
+const contentCard = 'Chúc mẹ ngày 20/10 thật nhiều điều bất ngờ và hạnh phúc, như những điều kỳ diệu mẹ đã mang đến cho cuộc đời con. Cảm ơn mẹ đã luôn là phép màu đẹp đẽ nhất trong cuộc đời con.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
